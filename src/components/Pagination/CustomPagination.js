3d1b8be76f3cf01880a9e08646fe8facf9ef4871
Jonathan Perea <john.perea00@gmail.com>
2022-02-23 21:42:56 -0700

    added filter option in movie menuthat actively filters the movies based on genre selected by user.

--- src/components/Pagination/CustomPagination.js
+++ src/components/Pagination/CustomPagination.js
@@ -1,8 +1,15 @@
-import { Pagination } from '@mui/material'
+import Pagination from "@material-ui/lab/Pagination";
 import React from 'react'
 import './CustomPagination.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function CustomPagination({setPage,numOfPages = 10}) {
+
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+    },
+  });
 
   const handlePageChange = (page) => {
       setPage(page);
@@ -11,11 +18,13 @@ export default function CustomPagination({setPage,numOfPages = 10}) {
 
   return (
     <div className='pagination' >
+      <ThemeProvider theme = {darkTheme}>
         <Pagination count = {numOfPages} color="primary" 
         onChange={(e) => handlePageChange(e.target.textContent)}
         hideNextButton
         hidePrevButton
         />
+      </ThemeProvider>
     </div>
   );
 };
